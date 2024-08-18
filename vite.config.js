6b6956d951ea7c9afea45e7968ba906a53ad8ebd
tzuhuangyen <912yan@gmail.com>
2024-08-18 14:40:44 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/React-shopCart/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        client: 'index.html', // Entry point for client-side code
+      },
+    },
+    outDir: 'dist',
+  },
+});
